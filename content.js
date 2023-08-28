57b00d92c765cd29a81c70b26554e55c4134719a
sonigeez <bharatchills@gmail.com>
2023-08-28 09:44:49 +0530

    carousal thing

--- content.js
+++ content.js
@@ -170,7 +170,124 @@ function amazonCode() {
       </div>
     </div>
     `;
-      grid.insertBefore(componentContainer, grid.children[1]);
+      const carousal = `<div class="carousel">
+    <div class="carousel-container">
+        <div class="carousel-post">${componentContainer.innerHTML}</div>
+        <div class="carousel-post">${componentContainer.innerHTML}</div>
+        <div class="carousel-post">${componentContainer.innerHTML}</div>
+        <div class="carousel-post">${componentContainer.innerHTML}</div>
+    </div>
+    <div class="carousel-controls">
+        <button id="prevBtn">&lt;</button>
+        <div class="carousel-dots"></div>
+        <button id="nextBtn">&gt;</button>
+    </div>
+</div>`;
+      const carouselCss = `
+.carousel {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+}
+
+.carousel-container {
+  display: flex;
+  transition: transform 0.5s ease-in-out;
+}
+
+.carousel-post {
+  flex: 0 0 100%;
+
+}
+
+.carousel-controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+}
+
+#prevBtn,
+#nextBtn {
+  font-size: 1.5rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.carousel-dots {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.dot {
+  width: 10px;
+  height: 10px;
+  background-color: gray;
+  border-radius: 50%;
+  margin: 0 5px;
+  cursor: pointer;
+}
+
+.active-dot {
+  background-color: black;
+}`;
+      const carouselDiv = document.createElement("div");
+      carouselDiv.innerHTML = carousal;
+
+      grid.insertBefore(carouselDiv, grid.children[1]);
+      var styleSheet = document.createElement("style");
+      styleSheet.innerText = carouselCss;
+      document.head.appendChild(styleSheet);
+      const carouselContainer = document.querySelector(".carousel-container");
+      const prevBtn = document.getElementById("prevBtn");
+      const nextBtn = document.getElementById("nextBtn");
+      const dotsContainer = document.querySelector(".carousel-dots");
+      const posts = document.querySelectorAll(".carousel-post");
+
+      let currentIndex = 0;
+
+      function updateCarousel() {
+        carouselContainer.style.transform = `translateX(-${
+          currentIndex * 100
+        }%)`;
+
+        const activeDot = dotsContainer.querySelector(".active-dot");
+        if (activeDot) {
+          activeDot.classList.remove("active-dot");
+        }
+        dotsContainer.children[currentIndex].classList.add("active-dot");
+      }
+
+      function moveToNext() {
+        currentIndex = (currentIndex + 1) % posts.length;
+        updateCarousel();
+      }
+
+      function moveToPrev() {
+        currentIndex = (currentIndex - 1 + posts.length) % posts.length;
+        updateCarousel();
+      }
+
+      function createDots() {
+        for (let i = 0; i < posts.length; i++) {
+          const dot = document.createElement("div");
+          dot.classList.add("dot");
+          if (i === currentIndex) {
+            dot.classList.add("active-dot");
+          }
+          dot.addEventListener("click", () => {
+            currentIndex = i;
+            updateCarousel();
+          });
+          dotsContainer.appendChild(dot);
+        }
+      }
+
+      nextBtn.addEventListener("click", moveToNext);
+      prevBtn.addEventListener("click", moveToPrev);
+      createDots();
     } else {
       console.log("Not Found");
     }
@@ -196,103 +313,169 @@ function googleCode() {
 function createUI(title, description, url, imageUrl) {
   let searchResult = document.querySelector(".g"); // Select only the first search result
   if (searchResult) {
-    // Check if the search result exists
-    // Outermost container
-    let container = document.createElement("div");
-    container.style.display = "flex";
-    container.style.justifyContent = "flex-start";
+    // a title, a subtitle, a image and button on trailing
+    const componentContainer = document.createElement("div");
+    componentContainer.innerHTML = `<div class="tile">
+    <div class="tile-content">
+      <h2 class="tile-title">${title}</h2>
+      <p class="tile-subtitle">${description}</p>
+      <a href="${url}">buy now</a>
+    </div>
+    <img class="tile-image" src=${imageUrl} alt="Trailing Image">
+  </div>`;
+  const componentCss = `  .tile {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
 
-    // Image
-    let img = document.createElement("img");
-    img.src =
-      "https://as2.ftcdn.net/v2/jpg/02/94/54/07/1000_F_294540722_VIwSWyrU6q781SkHWSlzUDXjfouRlj47.jpg"; // Placeholder image, replace with your own image source
-    img.alt = "Image description";
-    img.style.width = "30px";
-    img.style.height = "30px";
-    img.style.marginRight = "16px";
-    img.style.borderRadius = "50%";
-    // Text container
-    let textContainer = document.createElement("div");
-    // Text 1
-    let text1 = document.createElement("p");
-    text1.textContent = title;
-    text1.style.margin = "0";
-    text1.style.color = "#fffff"; // Google blue color
-    // Text 2
-    let text2 = document.createElement("p");
-    text2.textContent = url + " ";
-    text2.style.margin = "0";
-    text2.style.color = "#70757a"; // Google gray color
-    // Append text elements to the text container
-    textContainer.appendChild(text1);
-    textContainer.appendChild(text2);
-    // Append image and text container to the container
-    container.appendChild(img);
-    container.appendChild(textContainer);
-    // Insert the new element before the first child of the result
-    searchResult.insertBefore(container, searchResult.firstChild);
-    // Second container
-    let secondContainer = document.createElement("div");
-    // Text 3
-    let text3 = document.createElement("h3");
-    text3.textContent = title;
-    text3.style.margin = "0";
-    text3.style.color = "#87B1F2"; // Google blue color
-    // Text 4
-    let text4 = document.createElement("p");
-    text4.textContent = description;
-    text4.style.color = "#BDC1C5"; // Google gray color
-    let buttonContainer = document.createElement("div");
-    buttonContainer.style.display = "flex";
-    buttonContainer.style.gap = "10px";
-    buttonContainer.style.marginLeft = "auto"; // Add marginLeft
+  .tile-content {
+    flex-grow: 1;
+  }
 
-    // Button 1
-    let button1 = document.createElement("button");
-    button1.textContent = "Buy Now";
-    button1.style.padding = "10px";
-    button1.style.borderRadius = "5px";
-    button1.style.backgroundColor = "#047A55";
-    button1.style.color = "#ffffff";
-    button1.style.cursor = "pointer";
-    button1.style.border = "none";
+  .tile-title {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin: 0;
+  }
 
-    button1.addEventListener("click", function () {
-      // This will open the URL in a new tab when clicked
-      window.open(url, "_blank");
+  .tile-subtitle {
+    font-size: 1rem;
+    color: #666;
+    margin: 0;
+  }
 
-      // Send message to background script to save item
-      chrome.runtime.sendMessage({
-        type: "BUY_ITEM",
-        title: title,
-        url: url,
-      });
-    });
+  .tile-image {
+    flex-shrink: 0;
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+    object-fit: cover;
+    margin-left: 10px;
+  }`;
 
-    // Button 2
-    let button2 = document.createElement("button");
-    button2.textContent = "Remove it";
-    button2.style.padding = "10px";
-    button2.style.borderRadius = "5px";
-    button2.style.backgroundColor = "#C81E1D";
-    button2.style.color = "#ffffff";
-    button2.style.cursor = "pointer";
-    button2.style.border = "none";
-    button2.addEventListener("click", function () {
-      // This will remove the entire searchResult when clicked
-      console.log("Removing");
-      searchResult.removeChild(container);
-      searchResult.removeChild(secondContainer);
-    });
-    // Append buttons to the button container
-    buttonContainer.appendChild(button1);
-    buttonContainer.appendChild(button2);
+  const carousal = `<div class="carousel">
+  <div class="carousel-container">
+      <div class="carousel-post">${componentContainer.innerHTML}</div>
+      <div class="carousel-post">${componentContainer.innerHTML}</div>
+      <div class="carousel-post">${componentContainer.innerHTML}</div>
+      <div class="carousel-post">${componentContainer.innerHTML}</div>
+  </div>
+  <div class="carousel-controls">
+      <button id="prevBtn">&lt;</button>
+      <div class="carousel-dots"></div>
+      <button id="nextBtn">&gt;</button>
+  </div>
+</div>`;
+    const carouselCss = `
+.carousel {
+position: relative;
+width: 100%;
+overflow: hidden;
+}
 
-    // Append text elements to the second container
-    secondContainer.appendChild(text3);
-    secondContainer.appendChild(text4);
-    container.appendChild(buttonContainer); // This line is new
-    // Insert the new element after the first container
-    searchResult.insertBefore(secondContainer, container.nextSibling);
+.carousel-container {
+display: flex;
+transition: transform 0.5s ease-in-out;
+}
+
+.carousel-post {
+flex: 0 0 100%;
+
+}
+
+.carousel-controls {
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 1rem;
+}
+
+#prevBtn,
+#nextBtn {
+font-size: 1.5rem;
+background: none;
+border: none;
+cursor: pointer;
+}
+
+.carousel-dots {
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+.dot {
+width: 10px;
+height: 10px;
+background-color: gray;
+border-radius: 50%;
+margin: 0 5px;
+cursor: pointer;
+}
+
+.active-dot {
+background-color: black;
+}`;
+  
+  const styleSheet = document.createElement("style");
+  styleSheet.innerText = componentCss;
+  document.head.appendChild(styleSheet);
+  const styles = document.createElement("style");
+  styles.innerText = carouselCss;
+  document.head.appendChild(styles);
+
+  const carousalDiv = document.createElement("div");
+  carousalDiv.innerHTML = carousal;
+    searchResult.insertBefore(carousalDiv, searchResult.children[0]);
+    const carouselContainer = document.querySelector(".carousel-container");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    const dotsContainer = document.querySelector(".carousel-dots");
+    const posts = document.querySelectorAll(".carousel-post");
+
+    let currentIndex = 0;
+
+    function updateCarousel() {
+      carouselContainer.style.transform = `translateX(-${
+        currentIndex * 100
+      }%)`;
+
+      const activeDot = dotsContainer.querySelector(".active-dot");
+      if (activeDot) {
+        activeDot.classList.remove("active-dot");
+      }
+      dotsContainer.children[currentIndex].classList.add("active-dot");
+    }
+
+    function moveToNext() {
+      currentIndex = (currentIndex + 1) % posts.length;
+      updateCarousel();
+    }
+
+    function moveToPrev() {
+      currentIndex = (currentIndex - 1 + posts.length) % posts.length;
+      updateCarousel();
+    }
+
+    function createDots() {
+      for (let i = 0; i < posts.length; i++) {
+        const dot = document.createElement("div");
+        dot.classList.add("dot");
+        if (i === currentIndex) {
+          dot.classList.add("active-dot");
+        }
+        dot.addEventListener("click", () => {
+          currentIndex = i;
+          updateCarousel();
+        });
+        dotsContainer.appendChild(dot);
+      }
+    }
+
+    nextBtn.addEventListener("click", moveToNext);
+    prevBtn.addEventListener("click", moveToPrev);
+    createDots();
+
   }
 }
